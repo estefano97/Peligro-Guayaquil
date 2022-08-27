f8b7348fc6b00a551ea670f051aa2b901208033b
estefano97 <kaliestefano@gmail.com>
2022-08-27 14:15:03 -0500

    Update & Clean the project

---
+++ src/components/Pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1>Bienvenido a mi sitio!, dianita, has aqui el login pls, puedes crear tus propios componentes, para que el codigo no sea tan grande</h1>
+    </div>
+  )
+}
+
+export default Login;
