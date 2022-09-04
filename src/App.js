dac5467e98de11a47744da5c2cbc0b9bb986ec5a
Karolina-Diana19 <ortegadianakarolina@gmail.com>
2022-09-04 12:57:37 -0500

    i modfied styles in login and register page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./App.css"
 import { BrowserRouter,Route,Routes} from 'react-router-dom';
 import Login from "./components/Pages/Login/Login"
 import Register from './components/Pages/Register/Register';
