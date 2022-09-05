48adf7bc689445163a1ec98d33cac2071516a8bd
Karolina-Diana19 <ortegadianakarolina@gmail.com>
2022-09-05 16:02:34 -0500

    agregue la carpeta Segurity

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/Pages/Login/Login"
 import Register from './components/Pages/Register/Register';
 import GlobalContextProvider from './GlobalContext';
 import Main from './components/Pages/Main/Main';
+import Segurity from './components/Pages/Segurity/Segurity'
 
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/login"  element={<Login/>}/>
         <Route path="/register"  element={<Register/>}/>
         <Route path='/' element={<Main/>}/>
+        <Route path='/segurity' element={<Segurity/>}/>
       </Routes>
     </BrowserRouter>
   </GlobalContextProvider>
---
+++ src/components/Pages/Segurity/Segurity.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Segurity = () => {
+    return (
+      <div className="Segurity">
+          <div className="ContainerButton">
+          <p>holaaaaa</p>
+          <button className="ButtonSegurity">Registrar crimen </button>
+          </div>
+      </div>
+    )
+  };
+
+  export default Segurity;
